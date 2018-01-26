2f4ec35148b7f4edc346b0e0e20ae68b8cf4e6de
Nam Vu <namdv@grooo.vn>
2018-01-26 16:47:59 +0700

    Add Open URL Scheme

--- SimpleUIKit/AppDelegate.swift
+++ SimpleUIKit/AppDelegate.swift
@@ -42,7 +42,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        let urlString = url.absoluteString
+        print("url String: \(urlString)")
+        
+        return true
+    }
 }
 
